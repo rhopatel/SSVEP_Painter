c2d832b9b7f80ac58fb3a8e9ff19b5bf42198366
Kyle Lee <kylelee.sea@gmail.com>
2022-10-09 14:46:43 -0400

    Image placed

--- SSVEP-demo.py
+++ SSVEP-demo.py
@@ -4,15 +4,19 @@ import tkinter
 from PIL import ImageTk, Image
 
 def main():
+    # Root setup
     root = Tk()
-
     root.title("SSVEP_demo")
     root.geometry('1920x1080')
-    frame = Canvas(root, width=600, height=400)
+
+    # Create canvas
+    frame = Canvas(root, width=1920, height=1080)
     frame.pack()
     frame.place(anchor='center', relx=0.5, rely=0.5)
+
+    # Place images
     img = ImageTk.PhotoImage(Image.open("media/arrow.jpg"))
-    frame.create_image(10,10,anchor=NW,image=img)
+    frame.create_image(0,0,anchor=NW,image=img)
 
     root.mainloop()
 
