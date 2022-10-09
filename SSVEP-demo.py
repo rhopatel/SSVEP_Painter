9f01d006ef32942559230a1d13a9ffe51648eba9
rohanp12 <rohanp12@users.noreply.github.com>
2022-10-09 14:35:05 -0400

    demo

--- SSVEP-demo.py
+++ SSVEP-demo.py
@@ -1,8 +1,21 @@
 
 from tkinter import *
+import tkinter
 
 def main():
     print("hello")
+    # create root window
+    root = Tk()
+
+
+    # root window title and dimension
+    root.title("SSVEP_demo")
+    # Set geometry (widthxheight)
+    root.geometry('350x200')
+    
+    # all widgets will be here
+    # Execute Tkinter
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
