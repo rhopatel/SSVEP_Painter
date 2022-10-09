b04f2c82c9bc07a58b8333611a34a813b3407541
rohanp12 <rohanp12@users.noreply.github.com>
2022-10-09 15:30:52 -0400

    working flips

--- SSVEP-demo.py
+++ SSVEP-demo.py
@@ -6,33 +6,58 @@ from PIL import ImageTk, Image
 
 class SSVEP_demo(object):
     def __init__(self, root, filename):
-        frame = tk.Canvas(root, width=1920, height=1080)
-        frame.place(anchor='center', relx=0.5, rely=0.5)
-        frame.pack()
+        self.root = root
+        canvas = tk.Canvas(root, width=1920, height=1080)
+        self.canvas = canvas
+        self.filename = filename
 
-        # Place images
-        margins = 200
-        width = 1500
-        height = 1080
+        self.canvas.place(anchor='center', relx=0.5, rely=0.5)
+        self.canvas.pack()
 
-        img = ImageTk.PhotoImage(Image.open(filename))
+        #image = Image.open(filename)
+        #img = ImageTk.PhotoImage(image)
 
-        frame.create_image(margins, 170,anchor=NW,image=img)
-        frame.create_image(margins, 320,anchor=NW,image=img)
+        self.update = self.draw().__next__
+        root.after(100, self.update)
 
-        frame.create_image(margins, 550,anchor=NW,image=img)
-        frame.create_image(margins, 720,anchor=NW,image=img)
+    def draw(self):
+        image = Image.open(self.filename)
+        time = 0
+        while True:
+            if time %2 == 0:
+                img = ImageTk.PhotoImage(image.transpose(Image.FLIP_LEFT_RIGHT))
+            else:
+                img = ImageTk.PhotoImage(image)
 
-        frame.create_image(width-margins, 170,anchor=NW,image=img)
-        frame.create_image(width-margins, 320,anchor=NW,image=img)
 
-        frame.create_image(width-margins, 550,anchor=NW,image=img)
-        frame.create_image(width-margins, 720,anchor=NW,image=img)
+            #icon1 = self.canvas.create_image(
+            #    250, 250, image=tkimage)
+            # Place images
+            margins = 200
+            width = 1500
+            height = 1080
 
-        root.mainloop()
+            icon1 = self.canvas.create_image(margins, 170,anchor=NW,image=img)
+            icon2 = self.canvas.create_image(margins, 320,anchor=NW,image=img)
 
-        #self.update = self.draw().__next__
-        #root.after(100, self.update)
+            icon3= self.canvas.create_image(margins, 550,anchor=NW,image=img)
+            icon4= self.canvas.create_image(margins, 720,anchor=NW,image=img)
+
+            icon5= self.canvas.create_image(width-margins, 170,anchor=NW,image=img)
+            icon6 =self.canvas.create_image(width-margins, 320,anchor=NW,image=img)
+
+            icon7 = self.canvas.create_image(width-margins, 550,anchor=NW,image=img)
+            icon8 = self.canvas.create_image(width-margins, 720,anchor=NW,image=img)
+
+            icons= [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8]
+            self.root.after_idle(self.update)
+
+            yield
+
+            for icon in icons:
+                self.canvas.delete(icon)
+            time += 1
+            #angle %= 360
 
 
 def main():
@@ -42,6 +67,8 @@ def main():
     filename = "media/pencil-icon.png"
     root.geometry('1920x1080')
     app = SSVEP_demo(root, filename)
+    root.mainloop()
+
 
 if __name__ == "__main__":
     main()
